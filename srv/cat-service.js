ef09202cf7cdc09a288d0c8e231071cc472fb8cf
Nicolas Lau <nicolas.lau@zalaris.com>
2024-03-14 09:20:13 +0000

    headers test

---
+++ srv/cat-service.js
@@ -0,0 +1,26 @@
+
+const cds = require('@sap/cds')
+
+module.exports = async srv => { // called by server.js
+
+    const { Books } = srv.entities
+
+    srv.before('READ', Books, async req => {
+
+        console.log(req.http.req.headers)
+
+    })
+
+    srv.on('callBooksWithHeader', async req => {
+        // https://cap.cloud.sap/docs/node.js/core-services#srv-send-request
+
+        const result = await srv.send({
+            query: SELECT.from(Books),
+            headers: { some: 'header' }
+        })
+
+        return result
+
+    })
+
+}
